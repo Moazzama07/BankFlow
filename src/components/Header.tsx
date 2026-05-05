fbafc227c04503323c6c105d8df8e75c117234bb
moazzama <mozimughal33@gmail.com>
2026-05-05 18:47:56 +0500

    Chages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Search, Bell, Settings } from "lucide-react";
+import React from "react";
+import { Bell, Settings } from "lucide-react";
 import userImg from "../assets/user.svg";
 import { useNavigate } from "react-router-dom";
 
@@ -8,7 +8,6 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ pageTitle = "Overview" }) => {
-    const [searchValue, setSearchValue] = useState("");
     const navigate = useNavigate();
     return (
         <header className="flex items-center justify-between px-8 py-4 bg-white border-b border-gray-100 h-[80px]">
@@ -17,18 +16,6 @@ const Header: React.FC<HeaderProps> = ({ pageTitle = "Overview" }) => {
 
             {/* Right Side */}
             <div className="flex items-center gap-4">
-
-                {/* Search Bar */}
-                <div className="flex items-center gap-2 bg-[#F5F7FA] rounded-full px-4 py-2 w-[220px] font-sans">
-                    <Search size={16} className="text-gray-400" />
-                    <input
-                        type="text"
-                        placeholder="Search for something"
-                        value={searchValue}
-                        onChange={(e) => setSearchValue(e.target.value)}
-                        className="bg-transparent text-sm text-gray-500 placeholder-gray-400 outline-none w-full"
-                    />
-                </div>
 
                 {/* Settings Icon */}
                 <button onClick={() => navigate("/settings")} className="w-10 h-10 flex items-center justify-center rounded-full bg-[#F5F7FA] text-[#718EBF] hover:bg-[#E8F0FF] hover:text-[#2D60FF] transition">
